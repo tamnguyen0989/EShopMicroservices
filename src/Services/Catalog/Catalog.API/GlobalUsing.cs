83994dfb15e1f98ff0e3bfe9e29cd0c716c2e049
Tam Nguyen <tamnguyen0989@gmail.com>
2024-12-11 17:15:04 +0700

    07 015 Test Seeding Initial Data with Marten on PostgresDB for Catalog.API microservice

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,9 +2,10 @@
 global using Mapster;
 global using MediatR;
 global using Marten;
-global using BuildingBlocks.CQRS;
-global using Catalog.API.Models;
-global using Catalog.API.Exceptions;
 global using FluentValidation;
+global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
+global using Catalog.API.Data;
