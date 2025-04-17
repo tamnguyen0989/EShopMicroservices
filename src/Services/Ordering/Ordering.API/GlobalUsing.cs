3ddbfe21ec5c8256022b4b239b11e71287d22736
Tam Nguyen <tamnguyen0989@gmail.com>
2025-04-17 14:20:29 +0700

    17 017 Health Checks for Ordering Microservices with EF.Core and SQL Server

---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+global using BuildingBlocks.Exceptions.Handler;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
